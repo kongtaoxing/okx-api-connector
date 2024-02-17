8b32b487d78da8614946110e855fde14dedcbcda
kongtaoxing <xyf8658@163.com>
2024-02-17 12:57:10 +0800

    feat: finish spot order

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Node.js OKX (OKEX) API SDK
 
-[npm version](https://img.shields.io/npm/v/okx-api-connector)
+![npm version](https://img.shields.io/npm/v/okx-api-connector)
 
 ## Installation
 
@@ -14,10 +14,40 @@ npm install okx-api-connector
 // Create rest client
 const okxClient = new OKX(process.env.OKX_API_KEY, process.env.OKX_SECRET_KEY, process.env.OKX_API_PASSWD);
 
+// Create client with option
+const okxClient = new OKX(
+  process.env.OKX_API_KEY, 
+  process.env.OKX_SECRET_KEY, 
+  process.env.OKX_API_PASSWD,
+  {
+    baseURL: process.env.OKX_BASE_URL,  // Replace the base url
+    isTestClient: true,  // Config if the client is a test client
+    config: {
+      proxy: {
+        protocol: 'http',
+        host: '127.0.0.1',
+        port: 7890,
+      }
+    }
+  });
+
 // Get balance
 const balance = await okxClient.getBalance();
 console.log(balance.data.data[0].details);
 
-// Put trade
-client.newOrder(symbol, side, type, params);
+// Check specific token balance
+const balance = await okxClient.getBalance({
+  tokens: ['USDC', 'USDT']
+});
+
+// Put trade, all params please refer to [OKX api](https://www.okx.com/docs-v5/en/#order-book-trading-trade-post-place-order)
+okxClient.newOrder(instId, tdMode, side, ordType, otherParams);
+
+// Transfer assets
+okxClient.transfer({
+  ccy: 'USDT',
+  amt: '10',
+  from: '18',
+  to: '6',
+});
 ```
--- src/RestClient.js
+++ src/RestClient.js
@@ -1,37 +1,35 @@
 const axios = require('axios');
 const crypto = require('crypto');
-require("dotenv").config();
 
 class OKX {
-  constructor(apiKey, secretKey, password, test = false) {
+  constructor(apiKey, secretKey, password, options = {}) {
+    this.isTestClient = options.isTestClient || false;
     this.apiKey = apiKey;
     this.secretKey = secretKey;
-    this.test = test;
+    this.baseURL = options.baseURL || "https://aws.okx.com";
     this.client = axios.create({
-      baseURL: process.env.OKX_BASE_URL,
+      baseURL: this.baseURL,
       timeout: 20000,
       headers: {
         "Content-Type": "application/json",
         "OK-ACCESS-KEY": apiKey,
         "OK-ACCESS-PASSPHRASE": password,
-        ...(test && { 'x-simulated-trading': 1 })
+        ...(this.isTestClient && { 'x-simulated-trading': 1 })
       },
-      proxy: {
-        protocol: 'http',
-        host: '127.0.0.1',
-        port: 7890,
-      }
+      ...options.config
     });
 
     this.client.interceptors.request.use(function (config) {
       const method = config.method.toUpperCase();
-      const path = config.url;
+      const path = (config.url + (config.params ? '?' + new URLSearchParams(config.params).toString() : '')).replace('\%2C', '\,');
+      const body = config.data ? JSON.stringify(config.data) : '';
       const timestamp = new Date().toISOString();
 
       const hmac = crypto.createHmac('sha256', secretKey);
-      hmac.update(timestamp + method + path);
+      hmac.update(timestamp + method + path + body);
       const sign = hmac.digest('base64');
 
+      // Update headers
       config.headers["OK-ACCESS-TIMESTAMP"] = timestamp,
       config.headers['OK-ACCESS-SIGN'] = sign;
 
@@ -41,16 +39,33 @@ class OKX {
     });
   }
 
-  async getBalance() {
-    return await this.client.get('/api/v5/account/balance');
+  async getBalance({tokens = []}) {
+    if (tokens.length > 0) {
+      return this.client.get('/api/v5/account/balance', {
+        params: {
+          ccy: tokens.join(',')
+        }
+      });
+    }
+    return this.client.get('/api/v5/account/balance');
   }
 
-  async newOrder(symbol, side, type, params) {
-    return await this.client.post('/api/v5/trade/order', {
-      instId: symbol,
-      tdMode: 'cash',
+  async transfer({ ccy, amt, from, to, ...params }) {
+    return this.client.post('/api/v5/asset/transfer', {
+      ccy: ccy,
+      amt: amt,
+      from: from,
+      to: to,
+      ...params
+    });
+  }
+
+  async spotOrder({instId, tdMode, side, ordType, ...params}) {
+    return this.client.post('/api/v5/trade/order', {
+      instId: instId,
+      tdMode: tdMode,
       side: side,
-      ordType: type,
+      ordType: ordType,
       ...params
     });
   }
